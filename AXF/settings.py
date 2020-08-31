f02d0c8387ee862042fbd5856056cc9dec3c1077
ncepu-liudong <46965739+ncepu-liudong@users.noreply.github.com>
2020-08-31 17:49:56 +0800

    2020.8.31
    简单前端登录界面

--- AXF/settings.py
+++ AXF/settings.py
@@ -131,7 +131,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/statics/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'statics')
+]
 
 REST_FRAMEWORK = {
 #  'DEFAULT_PAGINATION_CLASS':
